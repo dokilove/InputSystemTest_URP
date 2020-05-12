18da12d8ff13a9acd3a7e1f22e476b9bb8a39652
dokilove <dokilove2@gmail.com>
2020-05-13 01:00:41 +0900

    InputTest

---
+++ Assets/Cube.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Cube : MonoBehaviour
+{
+    PlayerControls controls;
+
+    Vector2 move;
+    Vector2 rotate;
+
+    float rotateY;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.GamePlay.Grow.performed += ctx => Grow();
+
+        controls.GamePlay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.GamePlay.Move.canceled += ctx => move = Vector2.zero;
+
+        controls.GamePlay.Rotate.performed += ctx => rotate = ctx.ReadValue<Vector2>();
+        controls.GamePlay.Rotate.canceled += ctx => rotate = Vector2.zero;
+
+        controls.GamePlay.RotateY.performed += ctx => rotateY = ctx.ReadValue<float>();
+        controls.GamePlay.RotateY.canceled += ctx => rotateY = 0.0f;
+    }
+
+    private void Update()
+    {
+        Vector2 m = new Vector2(move.x, move.y) * Time.deltaTime * 5.0f;
+        transform.Translate(m, Space.World);
+
+        Vector2 r = new Vector2(rotate.y, rotate.x) * Time.deltaTime * 100.0f;
+        transform.Rotate(r, Space.World);
+
+        float rY = rotateY * Time.deltaTime * 100.0f;
+        transform.RotateAroundLocal(Vector3.up, rY);
+    }
+
+
+    void Grow()
+    {
+        transform.localScale *= 1.1f;
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,219 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerControls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""GamePlay"",
+            ""id"": ""b114d4f0-b62c-4254-bb22-7b0815507569"",
+            ""actions"": [
+                {
+                    ""name"": ""Grow"",
+                    ""type"": ""Button"",
+                    ""id"": ""b00d6ee6-0aeb-45dd-947e-ad5ee898dd2a"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""21d22144-8882-46a9-ab99-5a9180a4e130"",
+                    ""expectedControlType"": ""Stick"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Rotate"",
+                    ""type"": ""Value"",
+                    ""id"": ""05238dfa-41b8-4498-adf1-57cb730b7bd7"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""RotateY"",
+                    ""type"": ""Button"",
+                    ""id"": ""efd9ceaf-01d1-48f6-81f0-df4361ea6289"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""59492ad1-0d77-4dff-9250-315398fb5f2b"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Grow"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""e09ccf20-da04-4952-88ff-8812995ffbd5"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""cc8c2021-0342-4358-8995-bc643a82fe32"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f20c11b3-0301-436a-beb4-287582f3051b"",
+                    ""path"": ""<Gamepad>/rightTrigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""RotateY"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // GamePlay
+        m_GamePlay = asset.FindActionMap("GamePlay", throwIfNotFound: true);
+        m_GamePlay_Grow = m_GamePlay.FindAction("Grow", throwIfNotFound: true);
+        m_GamePlay_Move = m_GamePlay.FindAction("Move", throwIfNotFound: true);
+        m_GamePlay_Rotate = m_GamePlay.FindAction("Rotate", throwIfNotFound: true);
+        m_GamePlay_RotateY = m_GamePlay.FindAction("RotateY", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // GamePlay
+    private readonly InputActionMap m_GamePlay;
+    private IGamePlayActions m_GamePlayActionsCallbackInterface;
+    private readonly InputAction m_GamePlay_Grow;
+    private readonly InputAction m_GamePlay_Move;
+    private readonly InputAction m_GamePlay_Rotate;
+    private readonly InputAction m_GamePlay_RotateY;
+    public struct GamePlayActions
+    {
+        private @PlayerControls m_Wrapper;
+        public GamePlayActions(@PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Grow => m_Wrapper.m_GamePlay_Grow;
+        public InputAction @Move => m_Wrapper.m_GamePlay_Move;
+        public InputAction @Rotate => m_Wrapper.m_GamePlay_Rotate;
+        public InputAction @RotateY => m_Wrapper.m_GamePlay_RotateY;
+        public InputActionMap Get() { return m_Wrapper.m_GamePlay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GamePlayActions set) { return set.Get(); }
+        public void SetCallbacks(IGamePlayActions instance)
+        {
+            if (m_Wrapper.m_GamePlayActionsCallbackInterface != null)
+            {
+                @Grow.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnGrow;
+                @Grow.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnGrow;
+                @Grow.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnGrow;
+                @Move.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnMove;
+                @Rotate.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnRotate;
+                @Rotate.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnRotate;
+                @Rotate.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnRotate;
+                @RotateY.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnRotateY;
+                @RotateY.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnRotateY;
+                @RotateY.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnRotateY;
+            }
+            m_Wrapper.m_GamePlayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Grow.started += instance.OnGrow;
+                @Grow.performed += instance.OnGrow;
+                @Grow.canceled += instance.OnGrow;
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Rotate.started += instance.OnRotate;
+                @Rotate.performed += instance.OnRotate;
+                @Rotate.canceled += instance.OnRotate;
+                @RotateY.started += instance.OnRotateY;
+                @RotateY.performed += instance.OnRotateY;
+                @RotateY.canceled += instance.OnRotateY;
+            }
+        }
+    }
+    public GamePlayActions @GamePlay => new GamePlayActions(this);
+    public interface IGamePlayActions
+    {
+        void OnGrow(InputAction.CallbackContext context);
+        void OnMove(InputAction.CallbackContext context);
+        void OnRotate(InputAction.CallbackContext context);
+        void OnRotateY(InputAction.CallbackContext context);
+    }
+}
